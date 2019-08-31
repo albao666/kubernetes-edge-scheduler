1f598c0f6b958a33fceb252d55c09fe6d1511a4e
albao666 <zhaofh93@163.com>
2019-08-31 16:17:48 +0800

    debug

--- demo/start-custom-scheduler.sh
+++ demo/start-custom-scheduler.sh
@@ -3,4 +3,4 @@
 kubectl uncordon media-master
 kubectl create -f ../configs/custom-role.yaml
 kubectl create -f ../configs/custom-scheduler.yaml
-kubectl cordon media-master
+# kubectl cordon media-master
