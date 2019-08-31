ebdd270c5fdfac55e8e6369ccd556803c28d7909
albao <zhaofh93@163.com>
2019-08-31 15:04:11 +0800

    modify start***.sh file and add new file stop***.sh to quickly stop service

--- demo/start-custom-scheduler.sh
+++ demo/start-custom-scheduler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl uncordon master
+kubectl uncordon media-master
 kubectl create -f ../configs/custom-role.yaml
 kubectl create -f ../configs/custom-scheduler.yaml
-kubectl cordon master
+kubectl cordon media-master
---
+++ demo/stop-custom-scheduler.sh
@@ -0,0 +1,3 @@
+kubectl delete -f ../configs/custom-role.yaml
+kubectl delete -f ../configs/custom-scheduler.yaml
+kubectl uncordon media-master
